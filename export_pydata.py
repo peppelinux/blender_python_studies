3d44c2e3033450defbd3b2d1c335dee63242127e
peppelinux <peppelinux@yahoo.it>
2015-07-08 19:41:00 +0200

    Create export_pydata.py

---
+++ export_pydata.py
@@ -0,0 +1,15 @@
+def print_pydata(active_ob):
+    """
+      active_ob = bpy.context.scene.objects.active
+    """
+    data = active_ob.data     
+    verts = [(round(v.co[0],5),round(v.co[1],5),round(v.co[2],5)) for v in data.vertices]
+    edges = [(e.vertices[0],e.vertices[1]) for e in data.edges]
+    #faces = [(f.vertices[0],f.vertices[1],f.vertices[2],f.vertices[3]) for f in data.faces]
+    print('')
+    print(len(verts),'vertices in total')
+    print(len(edges),'edges in total')
+    #print(len(faces),'faces in total\n')
+    #print('from_pydata(',verts,',',edges,',',faces,')', sep='')
+    print('from_pydata(',verts,',',edges,',)', sep='')
+    return (verts, edges)
